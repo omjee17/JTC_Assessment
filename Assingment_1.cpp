08dce82fc19d1669b869da26864e97f34edc5a3b
omjee17 <singhomjee123@gmail.com>
2021-10-19 21:56:04 +0530

    Initial Commit

--- Assingment_1.cpp
+++ Assingment_1.cpp
@@ -12,61 +12,61 @@ using namespace std;
 
 
 class ticket{
-  
-  public:
-    int n;
-    int *arr;
-    int ticketId;
-    int price;
-   
-    
-    ticket(int n)
-    {
-        price=0;
-        this->n=n;
-        arr=new int[n];
-        
-        // for now I'm just taking a simple way to declare 
-        // a unique ticketId(so the ticketId can be same)
-        // later we can use different way to get different unique ticketId.
-        
-        ticketId=(n+price)%mod;
-    }
-    
-    void getAges(int age[])
-    {
-        for(int i=0;i<n;i++)
-        {
-            if(age[i]<=2)
-            {
-                price+=0;
-            }
-            else if(age[i]>2 && age[i]<18)
-            {
-                price+=100;
-            }
-            else if(age[i]>=18 && age[i]<60)
-            {
-                price+=500;   
-            }
-            else
-            {
-                price+=300;
-            }
-            
-            arr[i]=age[i];
-        }
-        
-    }
-    
-    void ShowTicketDetails(){
-        cout<<"ticketId: "<<ticketId<<endl;
-        cout<<"Number of guest: "<<n<<endl;
-        for(int i=0;i<n;i++)
-	    {
-	        cout<<"Guest "<<i+1<<" "<<"(age:"<<this->arr[i]<<")"<<endl;
-	    }
-    }
+  
+  public:
+    int n;
+    int *arr;
+    int ticketId;
+    int price;
+   
+    
+    ticket(int n)
+    {
+        price=0;
+        this->n=n;
+        arr=new int[n];
+        
+        // for now I'm just taking a simple way to declare 
+        // a unique ticketId(so the ticketId can be same)
+        // later we can use different way to get different unique ticketId.
+        
+        ticketId=(n+price)%mod;
+    }
+    
+    void getAges(int age[])
+    {
+        for(int i=0;i<n;i++)
+        {
+            if(age[i]<=2)
+            {
+                price+=0;
+            }
+            else if(age[i]>2 && age[i]<18)
+            {
+                price+=100;
+            }
+            else if(age[i]>=18 && age[i]<60)
+            {
+                price+=500;   
+            }
+            else
+            {
+                price+=300;
+            }
+            
+            arr[i]=age[i];
+        }
+        
+    }
+    
+    void ShowTicketDetails(){
+        cout<<"ticketId: "<<ticketId<<endl;
+        cout<<"Number of guest: "<<n<<endl;
+        for(int i=0;i<n;i++)
+	    {
+	        cout<<"Guest "<<i+1<<" "<<"(age:"<<this->arr[i]<<")"<<endl;
+	    }
+    }
 
 };
 
@@ -87,34 +87,34 @@ map<int,ticket>database;
 
 int main()
 {
-    
-    cout<<"!!!------------WELCOME TO ZOO------------!!!!"<<endl;
-    
-    ticket g1(5);
+    
+    cout<<"!!!------------WELCOME TO ZOO------------!!!!"<<endl;
+    
+    ticket g1(5);
 	int arr[]={10,2,30,14,25};
 	g1.getAges(arr);
-    	
-    cout<<"Your ticketid:"<<g1.ticketId<<" TicketPrice: "<<g1.price<<endl;
+    	
+    cout<<"Your ticketid:"<<g1.ticketId<<" TicketPrice: "<<g1.price<<endl;
 	
 	auto it=database.find(g1.ticketId);
 	if(it==database.end())
 	{
-	   // if ticketId is not found means valid ticketId then show ticket details
-	   // and add ticket in database
-	    int id=g1.ticketId;
-	    database.insert({id,g1});
-	    g1.ShowTicketDetails();
+	   // if ticketId is not found means valid ticketId then show ticket details
+	   // and add ticket in database
+	    int id=g1.ticketId;
+	    database.insert({id,g1});
+	    g1.ShowTicketDetails();
 	}
 	else 
 	{
-	   // if ticketId is already present in database then warning generated 
-	   
-	    cout<<"!!!!  Please get a Valid Ticket  !!!!"<<endl;
+	   // if ticketId is already present in database then warning generated 
+	   
+	    cout<<"!!!!  Please get a Valid Ticket  !!!!"<<endl;
 	}
 
-    return 0;
-    
-    
-    // Written by: Omjee
-    // Email id: singhomjee123@gmail.com
+    return 0;
+    
+    
+    // Written by: Omjee
+    // Email id: singhomjee123@gmail.com
 }
